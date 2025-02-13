97859c6c5f1030f4eb52e67ee586f8f618c53125
kaleb22 <kaleb22@hotmail.com>
2025-02-13 13:43:18 -0300

    feat(task-component): styling the date info with the date pipe

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { Task } from '../../../interfaces/task.interface';
 import { CardComponent } from '../../card/card.component';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
